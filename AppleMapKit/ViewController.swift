7259b0cd7b41639f5332bd7c76d8333f864f642a
Sam Wong <sam.wong.uk@gmail.com>
2014-11-04 17:34:10 -0800

    Initial Commit

---
+++ AppleMapKit/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AppleMapKit
+//
+//  Created by Sam Wong on 04/11/2014.
+//  Copyright (c) 2014 Sam Wong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
